800a823990f4a4171f5b816cc078746cabc76098
poxiaozheng <277492866@qq.com>
2021-10-10 23:30:07 +0800

    实现uri转bytes数组的方法

--- app/src/main/java/com/example/bact/util/CommonUtil.kt
+++ app/src/main/java/com/example/bact/util/CommonUtil.kt
@@ -2,6 +2,7 @@ package com.example.bact.util
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.net.Uri
 import android.util.Base64
 import java.io.File
 import java.io.FileInputStream
@@ -14,14 +15,18 @@ object CommonUtil {
     }
 
 
-    fun encodeFileBase64(FilePath: String): String? {
-        val file = File(FilePath)
-        val fi = FileInputStream(file)
-        val length = fi.available()
-        val data = ByteArray(length)
-        fi.read(data, 0, length)
-        fi.close()
-        return Base64.encodeToString(data, Base64.DEFAULT)
+    fun uriToByteArr(uri: Uri): ByteArray? {
+       val path = uri.path
+        if(path!=null){
+            val file = File(path)
+            val fi = FileInputStream(file)
+            val length = fi.available()
+            val data = ByteArray(length)
+            fi.read(data, 0, length)
+            fi.close()
+            return data
+        }
+        return null
     }
 
 }
